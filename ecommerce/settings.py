a6ee800beb324ba160f8bc30d1e394f88365ad94
Arpit Bansal <91982333+ARPIT422@users.noreply.github.com>
2024-07-25 01:08:28 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -118,8 +118,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "ecommerce"/"static",
-]
+  os.path.join(BASE_DIR,"static")
+] 
+
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR / 'media'
